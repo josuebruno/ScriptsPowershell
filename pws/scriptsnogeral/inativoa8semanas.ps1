f94bf2508d4ecb2c3b587dcf730273623f0d0a4c
Josué Soares Bruno <73852814+josuebruno@users.noreply.github.com>
2022-06-06 14:13:24 -0300

    Adicionei novos comandos.

--- pws/scriptsnogeral/inativoa8semanas.ps1
+++ pws/scriptsnogeral/inativoa8semanas.ps1
@@ -1,1 +1,6 @@
-﻿$qnt = dsquery computer -inactive 8 -limit 0
+#Lista usuaros inativos a 8 semanacas
+dsquery computer -inactive 8 -limit 0
+
+#Mostra a quantidade 
+$qnt = dsquery computer -inactive 8 -limit 0
+($qnt).count
