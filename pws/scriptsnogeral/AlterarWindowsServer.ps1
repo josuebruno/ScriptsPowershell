a2edcfe0e61e872a8dd2892bdbaa0c7b72024761
Josué Soares Bruno <73852814+josuebruno@users.noreply.github.com>
2022-07-12 16:01:27 -0300

    adicionando chaves genericas

--- pws/scriptsnogeral/AlterarWindowsServer.ps1
+++ pws/scriptsnogeral/AlterarWindowsServer.ps1
@@ -7,3 +7,10 @@ DISM /online /Get-CurrentEdition
 DISM /online /Set-Edition:ServerStandard /ProductKey:XXXXX-XXXXX-XXXXX-XXXXX-XXXXX /AcceptEula
 
 
+
+
+Chaves de licença (Product keys) para Windows
+Windows Edition	Product Key
+Windows Server 2012 R2 Standard	D2N9P-3P6X9-2R39C-7RTCD-MDVJX
+Windows Server 2012 R2 Datacenter	W3GGN-FT8W3-Y4M27-J84CP-Q3VJ9
+Windows Server 2012 R2 Storage Server Standard	H2K4M-QNKQ2-64699-FYQHD-2WDYT
